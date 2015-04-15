b91b7c96401e13b98d35f83c48063412b93531a0
Amencome <amencome@163.com>
2015-04-15 16:39:23 +0800

    项目

---
+++ NN/NN/AAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AAAppDelegate.h
+//  NN
+//
+//  Created by Amencome on 14-8-23.
+//  Copyright (c) 2014年 am. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NN/NN/AAViewController.h
@@ -0,0 +1,15 @@
+//
+//  AAViewController.h
+//  NN
+//
+//  Created by Amencome on 14-8-23.
+//  Copyright (c) 2014年 am. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <QuartzCore/QuartzCore.h>
+@interface AAViewController : UIViewController<UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIActionSheetDelegate>
+
+
+@end
